f799714b5a46ac8c84d7138c68f4a5f2a792c84b
Victor Ezekiel <eze99victor@gmail.com>
2024-07-08 10:00:33 +0100

    Apple division problem ----------------------
    resolved as backtracking
    selection criteria
     + start from the largest and next largest
     + increase using sorted array if and only if diff is unbalanced
    base case
     + empty array

---
+++ dsa_hard/apple_division.py
@@ -0,0 +1,42 @@
+import math
+import sys
+import pdb
+
+def apple_division(n, array):
+    if n == 1:
+        return array[0]
+    
+    elif n == 2:
+        return array[1] - array[0]
+
+    else:
+        return main(array[n - 2], array[n - 1], array[: n - 2])
+
+def main(l_sum, r_sum, rem):
+    if len(rem) == 0:
+        return abs(l_sum - r_sum)
+
+    else:
+        min_pro_return = math.inf
+        # choice making
+        for idx in range(len(rem)):
+            if l_sum < r_sum:
+                rem_holder = rem
+                val = rem_holder.pop(idx)
+                return main(l_sum + val, r_sum, rem_holder)
+
+            else:
+                rem_holder = rem
+                val = rem_holder.pop(idx)
+                return main(l_sum, r_sum + val, rem_holder)
+
+            if return_value < min_pro_return:
+                min_pro_return = return_value
+        
+        return min_pro_return
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline())
+    p = list(map(int, sys.stdin.readline().rstrip().split()))
+    print(apple_division(n, p))
+
